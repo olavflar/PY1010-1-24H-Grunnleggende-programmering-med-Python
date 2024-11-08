b2586a52e51a96848cf383648a854983cdddefd7
olala <70377508+olavflar@users.noreply.github.com>
2024-11-08 10:26:51 +0100

    litt fiksing og endirng her og der for å teste

--- oblig_01.py
+++ oblig_01.py
@@ -1,25 +1,30 @@
-# In
-km_per_år = 10000  # Antall kjørte kilometer per år (du kan endre denne verdien)
+""" Den første obligatoriske innlevering i PY1010 """
 
-# Kostnader
-forsikring_elbil = 5000
-forsikring_bensinbil = 7500
+# Antall kjørte kilometer per år
+km_per_år = 10000  
+
+""" Kostnader for begge biler"""
 trafikkforsikringsavgift_per_dag = 8.38
-antall_dager_per_år = 365
-trafikkforsikringsavgift_aar = trafikkforsikringsavgift_per_dag * antall_dager_per_år
+trafikkforsikringsavgift_år = 8.38 * 365
 
-# Elbil kostnader
+""" Kostnader for elbil """
+forsikring_elbil = 5000
 elbil_drivstoffbruk_kwh_per_km = 0.2
 strompris_per_kwh = 2.0
-elbil_drivstoffkostnad = km_per_år * elbil_drivstoffbruk_kwh_per_km * strompris_per_kwh
 elbil_bomavgift = km_per_år * 0.1
-elbil_totalkostnad = forsikring_elbil + trafikkforsikringsavgift_aar + elbil_drivstoffkostnad + elbil_bomavgift
+elbil_drivstoffkostnad = km_per_år * elbil_drivstoffbruk_kwh_per_km * strompris_per_kwh
 
-# Bensinbil kostnader
+# Elbil totalkostnad
+elbil_totalkostnad = forsikring_elbil + trafikkforsikringsavgift_år + elbil_drivstoffkostnad + elbil_bomavgift
+
+""" Her er en oversikt over kostnader som kun gjelder for bensinbil  """
+forsikring_bensinbil = 7500
 bensinbil_drivstoffkostnad_per_km = 1.0
 bensinbil_drivstoffkostnad = km_per_år * bensinbil_drivstoffkostnad_per_km
 bensinbil_bomavgift = km_per_år * 0.3
-bensinbil_totalkostnad = forsikring_bensinbil + trafikkforsikringsavgift_aar + bensinbil_drivstoffkostnad + bensinbil_bomavgift
+
+# Bensinbil totalkostnad
+bensinbil_totalkostnad = forsikring_bensinbil + trafikkforsikringsavgift_år + bensinbil_drivstoffkostnad + bensinbil_bomavgift
 
 # Kostnadsdifferanse
 kostnadsdifferanse = bensinbil_totalkostnad - elbil_totalkostnad
