ba3f8bbf4c2abb0ab64a2daf87b3461b20013af0
olala <70377508+olavflar@users.noreply.github.com>
2025-01-16 10:17:58 +0100

    Add files via upload

---
+++ oblig_02_Oppg_01.py
@@ -0,0 +1,7 @@
+# 1: Alderberegning
+fødselsår = int(input("Skriv inn fødselsåret ditt: "))
+år_2024 = 2024
+alder_i_2024 = år_2024 - fødselsår
+
+print(f"Personen blir {alder_i_2024} år gammel i løpet av år {år_2024}.")
+
