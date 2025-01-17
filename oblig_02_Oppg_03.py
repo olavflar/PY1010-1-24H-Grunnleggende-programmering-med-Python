59aff6c8264286eaa0d96b1c27fd018258daeb9c
olala <70377508+olavflar@users.noreply.github.com>
2025-01-17 19:16:24 +0100

    Add files via upload

---
+++ oblig_02_Oppg_03.py
@@ -0,0 +1,7 @@
+#3: Fra grader til radianer
+import numpy as np
+
+v_grad = float(input("Skriv inn gradtallet: "))
+v_rad = v_grad * np.pi / 180
+
+print(f"{v_grad} grader tilsvarer {v_rad:.4f} radianer.")
